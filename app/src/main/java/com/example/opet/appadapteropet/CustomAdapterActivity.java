cf19ba838ef824de28548cd6a8cb4ab86c10d58a
opet <opet@Academico.edu>
2018-09-12 08:31:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/opet/appadapteropet/CustomAdapterActivity.java
@@ -0,0 +1,13 @@
+package com.example.opet.appadapteropet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomAdapterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_adapter);
+    }
+}
